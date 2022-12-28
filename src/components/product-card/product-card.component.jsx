984ace14cd0cf450be5e92725bdb6f1164b65a3e
GitnGeek <5278234+GitnGeek@users.noreply.github.com>
2022-12-28 19:49:43 +0100

    adding cart context, shopping icon and cart dropdown

---
+++ src/components/product-card/product-card.component.jsx
@@ -0,0 +1,24 @@
+import Button from '../button/button.component';
+import './product-card.styles.scss';
+
+const ProductCard = ({ product }) => {
+  const { name, imageUrl, price } = product;
+  return (
+    <div className='product-card-container'>
+      <img src={imageUrl} alt={`${name}`} />
+      <div className='footer'>
+        <span className='name'>{name}</span>
+        <span className='price'>{price}</span>
+      </div>
+      <Button
+        type='button'
+        buttonType='inverted'
+        onClick={() => alert('ADDED')}
+      >
+        ADD TO CART
+      </Button>
+    </div>
+  );
+};
+
+export default ProductCard;
