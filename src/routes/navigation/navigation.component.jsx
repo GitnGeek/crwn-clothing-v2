ca52edf0ab2bcbada3ec28e81a3346173fb0aecf
GitnGeek <5278234+GitnGeek@users.noreply.github.com>
2022-12-30 18:58:04 +0100

    added in our addToCart feature

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -15,7 +15,7 @@ import './navigation.styles.scss';
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
   const { isCartOpen } = useContext(CartContext);
-  console.log(isCartOpen);
+
   return (
     <Fragment>
       <div className='navigation'>
