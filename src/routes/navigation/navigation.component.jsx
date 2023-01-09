4db794a4929bbef1c86b19db61ed66d7f302e8fb
GitnGeek <5278234+GitnGeek@users.noreply.github.com>
2023-01-09 09:01:53 +0100

    Finishing redux migration for the user context

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,14 +1,14 @@
 import { Fragment, useContext } from 'react';
 import { Outlet } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 import CartIcon from '../../components/cart-icon/cart-icon.component';
 import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
 
-import { UserContext } from '../../contexts/user.context';
 import { CartContext } from '../../contexts/cart.context';
+import { selectCurrentUser } from '../../store/user/user.selector';
 
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
-
 import { signOutUser } from '../../utils/firebase/firebase.utils';
 
 import {
@@ -19,8 +19,9 @@ import {
 } from './navigation.styles';
 
 const Navigation = () => {
-  const { currentUser } = useContext(UserContext);
   const { isCartOpen } = useContext(CartContext);
+  const currentUser = useSelector(selectCurrentUser);
+  console.log('Navigation', currentUser);
 
   return (
     <Fragment>
