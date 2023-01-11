ca009e732b43675b0e851f87ad76d8d5c5c5888b
GitnGeek <5278234+GitnGeek@users.noreply.github.com>
2023-01-11 06:08:14 +0100

    move category business logic to selector and explain logging middlewre

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -5,17 +5,17 @@ import { useDispatch } from 'react-redux';
 import CategoriesPreview from '../categories-preview/categories-preview.component';
 import Category from '../category/category.component';
 import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils.js';
-import { setCategoriesMap } from '../../store/categories/category.action';
+import { setCategories } from '../../store/categories/category.action';
 
 const Shop = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const getCategoriesMap = async () => {
-      const categoryMap = await getCategoriesAndDocuments('categories');
-      dispatch(setCategoriesMap(categoryMap));
+    const getCategories = async () => {
+      const categoriesArray = await getCategoriesAndDocuments('categories');
+      dispatch(setCategories(categoriesArray));
     };
-    getCategoriesMap();
+    getCategories();
   }, [dispatch]); //[] just to avoid eslint warning
 
   return (
