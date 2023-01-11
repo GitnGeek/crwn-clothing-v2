21b882c3de73c32465acc4b6f0e9902df8622db6
GitnGeek <5278234+GitnGeek@users.noreply.github.com>
2023-01-11 06:44:53 +0100

    adding reselect

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,8 +1,26 @@
-export const selectCategoriesMap = state => {
-  console.log('selector fired');
-  return state.categories.categories.reduce((acc, category) => {
-    const { title, items } = category;
-    acc[title.toLowerCase()] = items;
-    return acc;
-  }, {});
+import { createSelector } from 'reselect';
+
+const selectCategoryReducer = state => {
+  console.log('selectCategoryReducer fired');
+  return state.categories;
 };
+
+export const selectCategories = createSelector(
+  [selectCategoryReducer],
+  categoriesSlice => {
+    console.log('selectCategories fired');
+    return categoriesSlice.categories;
+  }
+);
+
+export const selectCategoriesMap = createSelector(
+  [selectCategories],
+  categories => {
+    console.log('selectCategoriesMap fired');
+    return categories.reduce((acc, category) => {
+      const { title, items } = category;
+      acc[title.toLowerCase()] = items;
+      return acc;
+    }, {});
+  }
+);
