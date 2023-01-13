cbe066912286f7f8465cebe3d6163d8f0a4d8ae2
GitnGeek <5278234+GitnGeek@users.noreply.github.com>
2023-01-13 15:05:40 +0100

    redux thunk

--- src/store/store.js
+++ src/store/store.js
@@ -3,12 +3,14 @@ import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import logger from 'redux-logger';
 //import { loggerMiddleware } from './middleware/logger';
+import thunk from 'redux-thunk';
 
 import { rootReducer } from './root-reducer';
 
-const middleWares = [process.env.NODE_ENV === 'development' && logger].filter(
-  Boolean
-);
+const middleWares = [
+  process.env.NODE_ENV === 'development' && logger,
+  thunk
+].filter(Boolean);
 
 const composeEnhancer =
   (process.env.NODE_ENV !== 'production' &&
@@ -19,7 +21,7 @@ const composeEnhancer =
 const persistConfig = {
   key: 'root',
   storage,
-  blacklist: ['user']
+  whitelist: ['cart']
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
